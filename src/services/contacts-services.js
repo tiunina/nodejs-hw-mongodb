1551a25b3a1f4f94b96668ae90d65437173efeff
Alona Tiunina <163348743+tiunina@users.noreply.github.com>
2024-12-11 15:56:07 +0200

    fix name file

--- src/services/contacts-services.js
+++ src/services/contacts-services.js
@@ -1,4 +1,4 @@
-import ContactCollection from '../db/models/contacts.js';
+import ContactCollection from '../db/models/Contacts.js';
 
 export const getContacts = () => ContactCollection.find();
 export const getContactById = (id) => ContactCollection.findById(id);
